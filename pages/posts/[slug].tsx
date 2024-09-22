87668f4669954e84ca70659bd75e29d72a4c142f
Maxwell Burson <maxwellburson@gmail.com>
2024-09-22 00:47:40 -0700

    Fix codeblock margin

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -10,7 +10,7 @@ import { markdownToHtml } from '../../lib/markdownToHtml';
 import { IPost } from '../../types';
 import { DateFormatter } from '../../components/date-formatter';
 
-import style from './style.module.scss';
+import style from './posts.module.scss';
 import 'prismjs/themes/prism-okaidia.css';
 
 interface Props {
