6c10f6cf6e1af179198ce0dcdabd8e494c36b47a
Maxwell Burson <maxwellburson@gmail.com>
2024-09-28 16:22:03 -0700

    Trying stuff

--- app.config.ts
+++ app.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from '@solidjs/start/config';
 // https://docs.solidjs.com/solid-start/reference/entrypoints/app-config
 
 export default defineConfig({
+  ssr: true,
   // https://nitro.unjs.io/config
   // https://vinxi.vercel.app/guide/getting-started.html
   server: {
+    preset: 'static',
     prerender: {
       autoSubfolderIndex: true,
       crawlLinks: true,
