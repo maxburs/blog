5a26c2f8d200a51f3b81e0bad6b1cf73ea9d8418
Maxwell Burson <maxwellburson@gmail.com>
2024-10-03 22:51:00 -0700

    Replace back/forward buttons with "all posts"

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -4,7 +4,7 @@ import type { Metadata, ResolvingMetadata } from 'next';
 import * as constants from '../../../constants';
 
 import { Layout } from '../../../components/layout/layout';
-import { getPostBySlug, getAllPosts, getPostSlugs } from '../../../lib/api';
+import { getPostBySlug, getPostSlugs } from '../../../lib/api';
 import { markdownToHtml } from '../../../lib/markdownToHtml';
 import type { IPost } from '../../../types';
 import { DateFormatter } from '../../../components/date-formatter';
@@ -22,8 +22,8 @@ export async function generateStaticParams() {
 
 interface PageData {
   post: IPost;
-  nextPost: null | Pick<IPost, 'slug' | 'title'>;
-  lastPost: null | Pick<IPost, 'slug' | 'title'>;
+  // nextPost: null | Pick<IPost, 'slug' | 'title'>;
+  // lastPost: null | Pick<IPost, 'slug' | 'title'>;
 }
 
 async function getPageData(slug: string): Promise<PageData> {
@@ -33,18 +33,18 @@ async function getPageData(slug: string): Promise<PageData> {
   }
   const content = await markdownToHtml(post.content);
 
-  const allPosts = getAllPosts();
-  const index = allPosts.findIndex((p) => p.slug == post.slug);
-  if (index === -1) {
-    throw new Error();
-  }
-  const lastPost = allPosts[index - 1] ?? null;
-  const nextPost = allPosts[index + 1] ?? null;
+  // const allPosts = getAllPosts();
+  // const index = allPosts.findIndex((p) => p.slug == post.slug);
+  // if (index === -1) {
+  //   throw new Error();
+  // }
+  // const lastPost = allPosts[index - 1] ?? null;
+  // const nextPost = allPosts[index + 1] ?? null;
 
   return {
     post: { ...post, content },
-    lastPost: lastPost && { slug: lastPost.slug, title: lastPost.title },
-    nextPost: nextPost && { slug: nextPost.slug, title: nextPost.title },
+    // lastPost: lastPost && { slug: lastPost.slug, title: lastPost.title },
+    // nextPost: nextPost && { slug: nextPost.slug, title: nextPost.title },
   };
 }
 
@@ -74,7 +74,7 @@ export async function generateMetadata(
 }
 
 export default async function Post({ params }: RouteProps) {
-  const { post, lastPost, nextPost } = await getPageData(params.slug);
+  const { post /* , lastPost, nextPost */ } = await getPageData(params.slug);
 
   return (
     <Layout mainProps={{ className: styles.main }}>
@@ -88,22 +88,9 @@ export default async function Post({ params }: RouteProps) {
           dangerouslySetInnerHTML={{ __html: post.content }}
         />
       </article>
-      <ul className={styles.nav}>
-        <li>
-          {lastPost && (
-            <Link href={`/posts/${lastPost.slug}`} rel="prev">
-              ←{lastPost.title}
-            </Link>
-          )}
-        </li>
-        <li>
-          {nextPost && (
-            <Link href={`/posts/${nextPost.slug}`} rel="prev">
-              {nextPost.title}→
-            </Link>
-          )}
-        </li>
-      </ul>
+      <Link className={styles.allPostsLink} href="/">
+        ← All posts
+      </Link>
     </Layout>
   );
 }
