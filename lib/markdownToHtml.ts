68d581b373373c33d6e4739b75a5b7f877dfb205
Maxwell Burson <maxwellburson@gmail.com>
2024-10-06 17:23:44 -0700

    Add github markdown plugin

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,9 +1,11 @@
 import remarkPrismjs from 'gatsby-remark-prismjs';
 import { remark } from 'remark';
 import html from 'remark-html';
+import gfm from 'remark-gfm';
 
 export async function markdownToHtml(markdown: string) {
   const result = await remark()
+    .use(gfm)
     .use(html, { sanitize: false })
     .use(() => (tree) => {
       remarkPrismjs({ markdownAST: tree });
